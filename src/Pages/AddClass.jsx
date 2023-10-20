fc405c1568cc4d4cd6c7a1a263aec90946668401
Raj <rajan.lee97@gmail.com>
2023-10-20 19:44:05 -0400

    added buttons and forms to add class page

--- src/Pages/AddClass.jsx
+++ src/Pages/AddClass.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import './AddClass.css'
-
+import { Link } from 'react-router-dom'
 function AddClass() {
 
   return (
-      <>
-        <div>
-            class name
-        </div>
-        <div>
-            period
-        </div>
-      </>
+    <div id = "addClassPage">
+      <Link to = "/"><button>Back to HomePage</button></Link>
+      <br></br>
+      
+      <form id = "classForm">
+        <p>Class Name</p>
+        <input type = "text" name = "className"></input>
+        <p>Class Period</p>
+        <input type = "number" min="0" name = "classPeriod"></input>
+        <br></br>
+        <button id = "classSubmit" type ='submit'>Add class</button>
+      </form>
+    </div>
   );
 }
 
